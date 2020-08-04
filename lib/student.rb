95f54e94e5e617ff9b79e34314e1b94145bd8295
An Pham <anpham148@gmail.com>
2020-08-04 13:03:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,8 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |student|
-       @name = student[:name] 
+       @name = student[:name]
+       @location = student[:location]
     end
 
 
