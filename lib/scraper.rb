0eed68abca28ccde2e4c7784f175ea1ae26ecb6f
An Pham <anpham148@gmail.com>
2020-08-04 10:57:25 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,9 +11,10 @@ class Scraper
     scrapping = []
     cards = doc.css(".student-card")
     name = info.css(".student-name")[0].children.text
+    binding.pry
     location = info.css(".student-location")[0].children.text
     profile_url = info.css("a href")
-    binding.pry
+
 
     cards.each do |info|
 
