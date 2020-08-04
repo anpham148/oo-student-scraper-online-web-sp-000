4d43b7980a9f189afb5fe69fbfffa21a8dcdd294
An Pham <anpham148@gmail.com>
2020-08-04 10:45:25 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,26 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+
+    cards = doc.css(".student-card")
+
+    cards.each do |info|
+      name = info.css(".student-name")[0].children.text
+      location = info.css(".student-location")[0].children.text
+      profile = info.css("a href")
+      binding.pry
+    end
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
