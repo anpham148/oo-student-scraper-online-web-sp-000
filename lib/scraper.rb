7c367a2632df36bffdfe3fdd37d731c42e117739
An Pham <anpham148@gmail.com>
2020-08-04 11:43:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,8 +27,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
      html = open(profile_url)
      doc = Nokorgiri::HTML(html)
-
-
+     
     social_info = doc.css(".social-icon-container")
     vital_info = doc.css(".vitals-container")
     detail_info = doc.css(".details-container")
