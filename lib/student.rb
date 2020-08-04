8ae82100d6967d7b0d756ce5502b1af2ab6a742b
An Pham <anpham148@gmail.com>
2020-08-04 13:18:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,16 +19,13 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    # add_attributes = attributes_hash.each do |attr|
-    #
-    # end
     @twitter = attributes_hash[:twitter]
     @linkedin = attributes_hash[:linkedin]
     @github = attributes_hash[:github]
     @blog = attributes_hash[:blog]
     @profile_quote = attributes_hash[:profile_quote]
     @bio = attributes_hash[:bio]
-    
+
   end
 
   def self.all
